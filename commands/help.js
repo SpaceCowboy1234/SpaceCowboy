c71f4772b159167bb2c0544db4f247d76cf02633
Swan <swangoosegames@gmail.com>
2018-05-01 17:31:05 +1200

    Changed help command so that it doesn't work if user has no perm
    Removes commands in the command list if user cannot use, and stopped user from using help + commandname to see info on the command as they cannot use it anyways

--- commands/help.js
+++ commands/help.js
@@ -3,22 +3,41 @@ exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
+
+    var commandList = new Array();
+    commandNames.forEach(command => {
+      var cmd = client.commands.get(command);
+      if (client.elevation(message) >= cmd.conf.permLevel) {
+        //filters through all commands, not displaying those the author cannot use
+        commandList.push(client.commands.get(command));
+      }
+    });
+
+    message.channel.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${commandList.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, { code: 'asciidoc' });
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      var aliases = new Array();
-      if(!command.conf.aliases[0]){
-        aliases = "None";
-      }else {
-          aliases =  command.conf.aliases.join(", ");
+      
+      //if author cannot use the command, do not show the help for it
+      if (client.elevation(message) < command.conf.permLevel) {
+        return message.channel.send(`You do not have access to this command!`);
       }
-      message.channel.send(`= Command: ${command.help.name} = \n${command.help.description}\nUsage: ` + settings.prefix + `${command.help.usage}` + '\nAlternatives: ' + aliases, {code:'asciidoc'});
+
+      var aliases = new Array();
+      if (!command.conf.aliases[0]) {
+        aliases = "None";
+      } else {
+        aliases = command.conf.aliases.join(", ");
+      }
+      message.channel.send(`= Command: ${command.help.name} = \n${command.help.description}\nUsage: ` + settings.prefix + `${command.help.usage}` + '\nAlternatives: ' + aliases, { code: 'asciidoc' });
     } else {
       message.channel.send(`Command: ${params[0]} not found. Please double check spelling!`);
     }
   }
+
+
+
 };
 
 exports.conf = {
