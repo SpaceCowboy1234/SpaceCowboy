2ff58445e70d452c6bcab8349f80d91e4c68c366
AussieGamer1994 <aussiegamer1994@gmail.com>
2018-05-02 19:08:46 +1000

    Time Command Update
    Changed Lapras to mapName
    added MooMoo Farm
    Updates on some information

--- commands/time.js
+++ commands/time.js
@@ -14,11 +14,11 @@ snekfetch.get(apifull).then(r => {
     let body = r.body
 
     const embed = new RichEmbed()
-    .setTitle(`Timed Events`)
-    .addField("Daily Reset", `${body.reset}`, true)
+    .setTitle(`Timed Events || ${body.today}`)
+    .addField("Daily Reset", `${body.dailyReset}`, true)
     .addField("Contests", `**Bug Contest:** ${body.bug}`, true)
-    .addField("S.S. Anne", `**To Olivine:** ${body.toOlivine}\n**To Vermilion:** ${body.toVermilion}`, true)
-    .addField("Maps", `**Lapras:** ${body.lapras}\n**Flooded Lake of Rage:** ${body.lakeRage} `, true)
+    .addField("S.S. Anne", `**To Olivine:** ${body.olivine}\n**To Vermilion:** ${body.vermilion}`, true)
+    .addField("Maps", `**Union Cave BF2:** ${body.unioncaveb2f}\n**Flooded Lake of Rage:** ${body.rage}\n**MooMoo Farm:** ${body.moomoofarm}`, true)
     message.channel.send("", {
         embed: embed
     }).catch(console.error)
